f4cfffd4fb163e590c841943af3ba19d753c36d1
Miyuu Sawada <231128@st.com>
2025-02-14 09:45:55 +0900

    debugEleser

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(debugShowCheckedModeBanner: false,
       title: 'g→cc変換',
       theme: ThemeData(
         colorScheme: const ColorScheme(
